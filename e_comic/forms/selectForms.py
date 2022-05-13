d2575aaa62828823b80e88dd438a32f42aa86000
kim-shun <kimura1417080@gmail.com>
2022-05-14 06:02:02 +0900

    不要なファイルの削除

--- e_comic/forms/selectForms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class SelectForm(forms.Form):
-    point = forms.fields.FloatField(min_value=0.00, max_value=5.00, widget=forms.widgets.TextInput)
-    select1 = forms.fields.ChoiceField(required=True, widget=forms.widgets.Select)
-    select2 = forms.fields.ChoiceField(required=True, widget=forms.widgets.Select)
-    select3 = forms.fields.ChoiceField(required=True, widget=forms.widgets.Select)
-
